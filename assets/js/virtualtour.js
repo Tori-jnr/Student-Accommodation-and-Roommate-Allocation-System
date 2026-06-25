e713719f792b0dc2ee2491cb0810be7973b10fc7
Salama-Shali <roselyne.mwalagho@strathmore.edu>
2026-06-25 17:03:16 +0300

    updated virtual tour js

--- assets/js/virtualtour.js
+++ assets/js/virtualtour.js
@@ -1,57 +1,50 @@
-// 1. Create a mini database of your tours
+// 1. Create a mini database of  tours
 const toursDatabase = [
     {
         id: 1,
         name: "Greenview Residence Tour",
         type: "Double Room",
-        panoramaUrl: "https://pannellum.org/images/alma.jpg" // Greenview image
+        panoramaUrl: "https://static.vecteezy.com/system/resources/thumbnails/012/054/071/small_2x/seamless-360-panorama-in-interior-of-bedroom-of-cheap-hostel-flat-or-apartments-with-chairs-and-table-in-equirectangular-projection-with-zenith-and-nadir-vr-ar-content-photo.jpg"
     },
     {
         id: 2,
         name: "Magharibi Courts Tour",
         type: "Single Room",
-        panoramaUrl: "https://pannellum.org/images/cerro-toco-0.jpg" // Karibu image
+        panoramaUrl: "https://static.vecteezy.com/system/resources/thumbnails/017/167/738/small_2x/seamless-360-panorama-in-interior-of-bedroom-of-cheap-hostel-flat-or-apartments-with-chairs-and-table-in-equirectangular-projection-with-zenith-and-nadir-vr-ar-content-photo.jpg" 
     },
     {
         id: 3,
         name: "Himalayan Hostel Tour",
         type: "Premium Studio",
-        panoramaUrl: "https://pannellum.org/images/bma-0.jpg" 
+        panoramaUrl: "https://static.vecteezy.com/system/resources/thumbnails/022/594/867/small_2x/full-seamless-spherical-hdri-360-panorama-view-in-interior-of-modern-bedroom-in-vip-homestead-or-apaetments-with-panoramic-windows-in-equirectangular-projection-photo.jpg" 
     },
 
     {
         id: 4,
         name: "Sunrise Hostels Tour",
         type: "Four Sharing",
-        panoramaUrl: "https://pannellum.org/images/alma.jpg" 
+        panoramaUrl: "https://image.shutterstock.com/image-photo/hdri-360-panorama-interior-cheapest-260nw-2565572725.jpg" 
+
     },
     {
         id: 5,
         name: "Scholar Heights Tour",
         type: "Single Room",
-        panoramaUrl: "https://cdn.polyhaven.com/asset_img/primary/relax_inn_seaview_suite.png?height=760&quality=95", // Scholar image
-        hotspots: [
-            {
-                "pitch": 10,
-                "yaw": 45,
-                "type": "info",
-                "text": "Click here to see the Kitchen!"
-            }
-        ]
-            },
+        panoramaUrl: "https://cdn.polyhaven.com/asset_img/primary/relax_inn_seaview_suite.png?height=760&quality=95"
+ },
         
     
     {
         id: 6,
         name: "5 Point Residences Tour",
         type: "Double Room",
-        panoramaUrl: "https://pannellum.org/images/bma-0.jpg" 
+        panoramaUrl: "https://static.vecteezy.com/system/resources/thumbnails/022/796/926/small_2x/full-360-hdri-panorama-in-interior-of-wooden-eco-bedroom-in-rustic-style-hostel-or-homestead-on-mansard-floor-in-equirectangular-projection-with-zenith-and-nadir-vr-ar-content-photo.jpg"
     },
     {
         id: 7,
         name: "Happy Living Tour",
         type: "Single Room",
-        panoramaUrl: "https://pannellum.org/images/cerro-toco-0.jpg" // Karibu image
+        panoramaUrl: "https://static.vecteezy.com/system/resources/thumbnails/012/054/084/small_2x/seamless-360-panorama-in-interior-of-bedroom-of-cheap-hostel-flat-or-apartments-with-chairs-and-table-in-equirectangular-projection-with-zenith-and-nadir-vr-ar-content-photo.jpg" 
     }
 ];
 
@@ -81,6 +74,9 @@ document.addEventListener("DOMContentLoaded", () => {
     pannellum.viewer('panorama', {
         "type": "equirectangular",
         "panorama": currentTour.panoramaUrl, // <--- This changes the picture!
-        "autoLoad": true
+        "autoLoad": true,
+        "autoRotate": -2,
+       "hfov": 110,
+
     });
 });
